2db6e6244826c16fb664992fffc164d05fb93d5d
badsparky <92095206+badsparky@users.noreply.github.com>
2023-03-07 16:50:34 +0900

    done

--- LetMeKnow/GoalSetter.xaml.cs
+++ LetMeKnow/GoalSetter.xaml.cs
@@ -13,25 +13,11 @@ public partial class GoalSetter : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-		
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
-
-    private void MainButton_Clicked(object sender, EventArgs e)
+    private void PutButton_Clicked(object sender, EventArgs e)
     {
 
     }
-    private void MinorButton_Clicked(object sender, EventArgs e)
-    {
 
-    }
 }
 
